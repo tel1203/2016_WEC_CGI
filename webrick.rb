7be29614d120c9ca0d4e926c9138c824575ca0a2
Teruaki Yokoyama <tel1203@gmail.com>
2016-06-29 10:43:02 +0900

    modify webrick for CGI server.

--- webrick.rb
+++ webrick.rb
@@ -1,4 +1,13 @@
+#!/usr/bin/ruby
+
 require 'webrick'
+
+# 拡張子「.rb」ファイルをCGIとして実行可能にする。
+module WEBrick
+  module HTTPServlet
+    FileHandler.add_handler("rb", CGIHandler)
+  end 
+end
 
 # 動作設定
 opt = { 
@@ -7,6 +16,15 @@ opt = {
   :BindAddress    => nil 
 }
 server = WEBrick::HTTPServer.new(opt)
+
+# CGIを実行可能にする
+cgi_dir = File.dirname(File.expand_path(__FILE__))
+server.mount("/", WEBrick::HTTPServlet::FileHandler, cgi_dir)
+
+# サーバの終了シグナルを設定する
+['INT', 'TERM'].each {|signal| 
+  trap(signal) {server.shutdown}
+}
 
 # サーバを開始する
 server.start
