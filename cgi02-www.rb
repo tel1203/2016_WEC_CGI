fa586fcd6a1f34b248087f257ab53a54da0a73f7
Teruaki Yokoyama <tel1203@gmail.com>
2016-06-29 11:14:33 +0900

    program for HTML output

---
+++ cgi02-www.rb
@@ -0,0 +1,11 @@
+#! /Users/tel/.rbenv/shims/ruby
+
+printf("Content-Type: text/html\n\n")
+
+printf("<HTML>\n")
+printf("<HEAD><TITLE>CGIのテスト</TITLE></HEAD>\n")
+printf("<BODY>\n")
+printf("<A HREF='http://www.yahoo.co.jp/'>リンク</A>\n")
+printf("</BODY>\n")
+printf("</HTML>\n")
+
